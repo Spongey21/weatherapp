61332cf77e4f1ea9c3846a03699d858221edc53c
Spongey21 <driton.latifi101@gmail.com>
2023-12-15 13:46:06 +0100

    minor adjustments to clock and controls

--- src/components/clock.js
+++ src/components/clock.js
@@ -10,16 +10,17 @@ export default function clock() {
         navigator.geolocation.getCurrentPosition(async (position) => {
             setLocation(await GetLocation(position.coords.latitude, position.coords.longitude))
         });
+
     }, [])
 
     return (
-        <article className="flex flex-col justify-center text-center fixed top-[10%] left-[50%] translate-x-[-50%]">
-            <h1 className="text-white text-4xl">{!location.name ? location.name = 'unknown' : location.name}</h1>
-            <h2 className="text-white text-8xl">{location.main && Math.round(location.main.temp - 274.15)}°</h2>
-            <span className="text-gray-400">{location.location && location.location[0].description}</span>
-            <div className="flex justify-between">
-                <span className="text-white">H:{location.main && Math.round(location.main.temp_max - 274.15)}°</span>
-                <span className="text-white">L:{location.main && Math.round(location.main.temp_min - 274.15)}°</span>
+        <article className="flex flex-col justify-center text-center fixed top-[7.5%] left-[50%] translate-x-[-50%]">
+            <h1 className="text-white text-3xl capitalize">{!location.name ? location.name = 'unknown' : location.name}</h1>
+            <h2 className="text-white text-7xl">{location.main && Math.round(location.main.temp - 274.15)}°</h2>
+            <span className="text-gray-400 capitalize">{location.weather && location.weather[0].description}</span>
+            <div className="flex justify-between w-4/6 m-auto">
+                <span className="text-white capitalize">h:{location.main && Math.round(location.main.temp_max - 274.15)}°</span>
+                <span className="text-white capitalize">l:{location.main && Math.round(location.main.temp_min - 274.15)}°</span>
             </div>
         </article>
     )
--- src/components/controls.js
+++ src/components/controls.js
@@ -8,9 +8,9 @@ import IconBackground from '/public/Rectangle 364.svg'
 export default function Controls({ leftIcon, rightIcon, btnIcon }) {
     return (
         <>
-            <Image className='fixed bottom-0 left-[50%] translate-x-[-50%] z-10' src={ButtonBackground} alt='button background'/>
+            <Image className='fixed bottom-0 left-[50%] translate-x-[-50%] z-10' src={ButtonBackground} alt='button background' height={75}/>
             <Image className='fixed bottom-0' src={IconBackground} alt='icon background'/>
-            <FontAwesomeIcon className='text-purple-800 bg-white fixed bottom-[2.5%] left-[50%] translate-x-[-50%] rounded-full text-3xl p-5 z-10' icon={btnIcon} />
+            <FontAwesomeIcon className='text-purple-800 bg-white fixed bottom-[2.5%] left-[50%] translate-x-[-50%] rounded-full text-2xl p-3 z-10' icon={btnIcon} />
             <FontAwesomeIcon className='text-white fixed bottom-[3.5%] left-[10%] text-2xl z-10' icon={leftIcon} />
             <FontAwesomeIcon className='text-white fixed bottom-[3.5%] right-[10%] text-2xl z-10' icon={rightIcon} />
         </>
